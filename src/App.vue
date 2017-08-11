a1eafe411e30afb95273671bec558fb365613b17
PeggyWeb <15220037806@163.com>
2017-08-11 19:53:15 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <top-bar></top-bar>
+    <img src="./assets/img/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import Bar from './components/TopBar.vue'
+export default {
+    components:{
+        'top-bar':Bar
+      }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <header id="topbar">
+    <div class="container">
+      <div class="topbar-nav">
+        <a href="#">小米商城</a><a href="#">MIUI</a>
+      </div>
+      <div class="topbar-cart fr">
+        <a href="#" rel="nofollow"><span class="iconfont">&#xe698;</span>购物车</a>
+      </div>
+      <div class="topbar-info fr">
+        <a href="#">登录</a>
+        <a href="#">注册</a>
+        <span class="message">
+                    <a href="#" rel="nofollow">消息中心</a>
+                </span>
+      </div>
+    </div>
+  </header>
+</template>
+<style>
+
+</style>
+<script>
+
+</script>
